2a022ee0bb1f80fb6204f8a861eb606df3a1abd9
VictorSad26 <victor.sad@hotmail.com>
2021-06-24 23:25:35 -0300

    Trocando a porta

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
 })
